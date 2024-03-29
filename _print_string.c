a1cb23e3f8fd7f0b08a3124b9af6815ed9d580af
Stefani-web <stephaniecvlho@gmail.com>
2024-03-29 15:04:24 +0100

    corrig a final loop

--- _print_string.c
+++ _print_string.c
@@ -10,17 +10,23 @@
 int print_string(va_list args)
 {
 	char *s = va_arg(args, char *);
-	int len = 0;
-	int length = 0;
+	int len = 0, length = 0;
 
 	if (s == NULL)
 		s = "(null)";
 
 	while (s[len] != '\0')
+	{
 		len++;
+	}
+	
 	length = len;
+	len = 0;
 	while (len < length)
+	{
 		_putchar(s[len]);
+		len++;
+	}
 
 	return (len);
 }
