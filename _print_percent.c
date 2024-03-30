5af03b7a47f5071f598a8c390cc97ce1743956a5
Delphine-H <hannon.delphine@gmail.com>
2024-03-30 16:05:21 +0100

    change func argument to attribute unused va_list args i/o void

--- _print_percent.c
+++ _print_percent.c
@@ -2,11 +2,12 @@
 
 /**
  * print_percent - Prints a percent sign
+ * @args: va_list containing the integer to print
  *
  * Return: 1 (number of chars printed)
  */
 
-int print_percent(void)
+int print_percent(__attribute__((unused))va_list args)
 {
 	_putchar('%');
 	return (1);
