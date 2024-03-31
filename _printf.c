bbff1927f7bc9709378f6485e505a032b6d15102
Delphine-H <hannon.delphine@gmail.com>
2024-03-31 22:02:15 +0200

    cancel last commit

--- _printf.c
+++ _printf.c
@@ -18,9 +18,6 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
-	if (format[0] == '%' && format[1] == '\0')
-		return (0);
-
 	for (p = format; *p; p++)
 	{
 		if (*p == '%')
@@ -32,7 +29,18 @@ int _printf(const char *format, ...)
 			{
 				count += function(args);
 			}
-
+			else
+			{
+				if (p[1])
+				{
+					count += _putchar('%');
+					count += _putchar(*p);
+				}
+				else
+				{
+					return (0);
+				}
+			}
 		}
 		else
 		{
